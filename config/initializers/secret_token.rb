72848388140d7ba91467eea3304bdc94fd0c951b
Quin Cogdell <quincogdell@gmail.com>
2013-11-06 09:17:46 -0500

    empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ship2ship::Application.config.secret_token = '020fb4ebb128c8a4f7a4b3d4a0f9a4de971b69ffc42b35829f8f4b2cb49e132bf749b383c219e4d972502d9ee4b4da6792a792148e27c3e6d9df8f131e037704'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ship2ship::Application.config.session_store :cookie_store, key: '_ship2ship_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ship2ship::Application.config.session_store :active_record_store
